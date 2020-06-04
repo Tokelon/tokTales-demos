dff8c7862c54e7287ee193081b2adeab49f1f54e
Tokelon <tokelontales@gmail.com>
2020-06-04 19:34:39 +0200

    Upgrade tokTales-helloworld to tokTales 0.2.0-rc3

--- tokTales-helloworld/tokTales-helloworld-android/src/main/java/com/tokelon/toktales/helloworld/android/HelloWorldApp.java
+++ tokTales-helloworld/tokTales-helloworld-android/src/main/java/com/tokelon/toktales/helloworld/android/HelloWorldApp.java
@@ -1,20 +1,20 @@
 package com.tokelon.toktales.helloworld.android;
 
-import com.tokelon.toktales.android.app.TokTalesApp;
-import com.tokelon.toktales.android.engine.AndroidEngineLauncher;
-import com.tokelon.toktales.android.engine.inject.MasterAndroidInjectConfig;
-import com.tokelon.toktales.core.engine.EngineException;
-import com.tokelon.toktales.core.engine.IEngineLauncher;
+import com.tokelon.toktales.android.application.TokTalesApp;
+import com.tokelon.toktales.core.game.IGameAdapter;
 import com.tokelon.toktales.helloworld.core.HelloWorldAdapter;
 
 public class HelloWorldApp extends TokTalesApp {
+    /* Our application inherits from TokTalesApp, which will do all the work for us.
+     * We just have to tell it how to initialize the engine, using the makeDefault*() methods.
+     *
+     * Works by setting this class as the Application implementation for Android, in the AndroidManifest.
+     */
 
 
     @Override
-    protected void launchEngine(IEngineLauncher defaultLauncher) throws EngineException {
-        AndroidEngineLauncher launcher = new AndroidEngineLauncher(new MasterAndroidInjectConfig(), getApplicationContext());
-
-        launcher.launch(HelloWorldAdapter.class);
+    public Class<? extends IGameAdapter> makeDefaultGameAdapter() {
+        return HelloWorldAdapter.class;
     }
 
 }
--- tokTales-helloworld/tokTales-helloworld-desktop/src/main/java/com/tokelon/toktales/helloworld/desktop/HelloWorldMain.java
+++ tokTales-helloworld/tokTales-helloworld-desktop/src/main/java/com/tokelon/toktales/helloworld/desktop/HelloWorldMain.java
@@ -1,18 +1,35 @@
 package com.tokelon.toktales.helloworld.desktop;
 
 import com.tokelon.toktales.core.engine.EngineException;
-import com.tokelon.toktales.desktop.engine.inject.MasterDesktopInjectConfig;
-import com.tokelon.toktales.desktop.lwjgl.DefaultDesktopEngineLauncher;
+import com.tokelon.toktales.core.game.IGameAdapter;
+import com.tokelon.toktales.desktop.application.TokTalesApplication;
+import com.tokelon.toktales.desktop.ui.window.IWindowBuilder;
 import com.tokelon.toktales.helloworld.core.HelloWorldAdapter;
 
-public class HelloWorldMain {
+public class HelloWorldMain extends TokTalesApplication {
+    /* Our application inherits from TokTalesApplication, which will do all the work for us.
+     * We just have to tell it how to initialize the engine, using the makeDefault*() methods.
+     * For that we can utilize the provided factory getters like getWindowFactory().
+     *
+     * Works by calling run() for this class in the main method.
+     */
+
+
+    @Override
+    public IWindowBuilder makeDefaultWindowBuilder() {
+        // Define the size and title for the main application window
+        return getWindowFactory().createBuilder(800, 600, "Hello World");
+    }
+
+    @Override
+    public Class<? extends IGameAdapter> makeDefaultGameAdapter() {
+        return HelloWorldAdapter.class;
+    }
 
 
     public static void main(String[] args) throws EngineException {
-        DefaultDesktopEngineLauncher launcher = new DefaultDesktopEngineLauncher(new MasterDesktopInjectConfig());
-
-        launcher.useDefaultWindowBuilderWith(800, 600, "Hello World");
-        launcher.launch(HelloWorldAdapter.class);
+        // Run the application. This call will not return until the engine is stopped
+        new HelloWorldMain().run(args);
     }
 
 }
